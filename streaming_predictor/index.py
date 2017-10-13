8ba3f9f5445be502609bd4ab5a1343f4ad09a1f6
christopher snow <chsnow123@gmail.com>
2017-10-13 14:11:43 +0100

    add error checking

--- streaming_predictor/index.py
+++ streaming_predictor/index.py
@@ -7,7 +7,7 @@ logger.addHandler(logging.StreamHandler(sys.stdout))
 logger.setLevel(logging.INFO)
 
 from kafka import KafkaProducer, KafkaConsumer
-from kafka.errors import KafkaError
+from kafka.errors import KafkaError, KafkaTimeoutError
 from kafka.producer.future import RecordMetadata
 
 from collections import OrderedDict
@@ -75,9 +75,8 @@ def get_producer(opts):
                          ssl_context = context,
                          sasl_mechanism = sasl_mechanism,
                          api_version = (0,10),
-                         retries = 5,
+                         retries = 1,
                          key_serializer=str.encode,
-                         #value_serializer=lambda v: json.dumps(v).encode('utf-8')
                          )
     return producer
 
@@ -104,7 +103,7 @@ def kafka_send_callback(args):
         # Example error output:
         #    KafkaTimeoutError: ('Batch containing %s record(s) expired due to timeout while requesting metadata from brokers for %s', 49, TopicPartition(topic='transactions_load', partition=2))
     else:
-        print(args)
+        print('UNKNOWN ERROR: ' + str(args))
 
 producer = get_producer(opts)
 consumer = get_consumer(group_id='streaming_predictor-2', opts=opts)
@@ -117,33 +116,38 @@ consumer.subscribe([opts['topic-transactions']])
 
 print('Subscribed to: ' + opts['topic-transactions'])
 
-for msg in consumer:
+def process_loop():
+    for msg in consumer:
+    
+        #print('Processing: ' + str(msg))
+   
+        try:
+            j = json.loads(str(msg.value.decode('utf-8')))
 
-    #print('Processing: ' + str(msg))
+            transaction_id = j['TransactionID']
+            customer_id = j['CustomerID']
+            quantity = j['Quantity']
+            price = j['UnitPrice']
+            tx_datetime = j['InvoiceDate']
+        
+            data = np.array([[price, quantity, customer_id]])
+        
+            predict_prob = logistic.predict_proba(data)
+            #print(predict_prob)
+        
+            prob_cancelled = predict_prob[0][1]
 
-    j = json.loads(str(msg.value.decode('utf-8')))
+            prediction =  OrderedDict([    
+                    ('transaction_id', transaction_id),
+                    ('invoice_date',   tx_datetime),
+                    ('prob_cancelled', prob_cancelled)
+                    ])
 
-    transaction_id = j['TransactionID']
-    customer_id = j['CustomerID']
-    quantity = j['Quantity']
-    price = j['UnitPrice']
-    tx_datetime = j['InvoiceDate']
+            producer.send(opts['topic-predictions'], key=j['TransactionID'], value=json.dumps(prediction).encode('utf-8')).add_both(kafka_send_callback)
 
-    data = np.array([[price, quantity, customer_id]])
-
-    predict_prob = logistic.predict_proba(data)
-    #print(predict_prob)
-
-    prob_cancelled = predict_prob[0][1]
-
-    #if prob_cancelled > 0.5:
-
-    prediction =  OrderedDict([    
-            ('transaction_id', transaction_id),
-            ('invoice_date',   tx_datetime),
-            ('prob_cancelled', prob_cancelled)
-            ])
-
-    producer.send(opts['topic-predictions'], key=j['TransactionID'], value=json.dumps(prediction).encode('utf-8')).add_both(kafka_send_callback)
-
-
+        except Exception as e:
+            print('Error creating probability msg. Error: {} data: {}'.format(str(e), str(msg.value.decode('utf-8'))))
+    
+    
+if __name__ == "__main__":
+    process_loop()
